6e64848373a34ce9778a585e192b828632a6e1be
AmalCM27 <azamatovamal5@gmail.com>
2024-05-14 00:11:48 +0600

    the create menu

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './main.css';
 import SideMenu from '../../components/sideMenu/SideMenu';
+import Header from '../header/Header';
 
 function Main() {
   return (
     <main>
       <SideMenu />
       <div className="banner">
-        
+        <Header/>
       </div>
     </main>
   );
