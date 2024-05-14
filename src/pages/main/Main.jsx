20509919da3eb55b40c875161a294e6c90ab22f9
AmalCM27 <azamatovamal5@gmail.com>
2024-05-14 23:30:17 +0600

    end of this working day

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './main.css';
 import SideMenu from '../../components/sideMenu/SideMenu';
 import Header from '../header/Header';
+import Home from '../home/Home';
 
 function Main() {
   const [active, setActive] = useState(false);
+  const [games, setGames] = useState([]);
 
   const handelToggleActive = () => {
     setActive(!active);
+  };
+
+  const fetchData = () => {
+    fetch('http://localhost:3000/api/gamesData.json')
+      .then(res => res.json())
+      .then(data => {
+        setGames(data);
+      })
+      .catch(e => console.log(e.message));
   }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <main>
       <SideMenu active={active} />
       <div className={`banner ${active ? 'active' : undefined}`}>
         <Header toggelActive={handelToggleActive} />
+        <div className="container-fliud">
+          <Home games={games} />
+        </div>
       </div>
     </main>
   );
